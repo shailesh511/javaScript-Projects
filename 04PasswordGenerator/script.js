cf34d1a7decdd7fa8861c6bf297ada47c7312d9d
Shailesh <shaileshtripathi511@gmail.com>
2022-09-26 00:19:20 +0530

    Project-4 js added

--- 04PasswordGenerator/script.js
+++ 04PasswordGenerator/script.js
@@ -19,21 +19,66 @@ clipboardEl.addEventListener('click', () => {
 })
 
 generateEl.addEventListener('click', () => {
-    
+    let strongPass="";
+    let length=0;
+
+    strongPass = generatePassword(randomFunc.lower, randomFunc.upper, randomFunc.number, randomFunc.symbol, length);
+    document.getElementById("result").innerText=strongPass;
 })
 
 function generatePassword(lower, upper, number, symbol, length) {
-    
+    let v1=lower();
+    let v2=upper();
+    let v3=number();
+    let v4=symbol();
+
+    let pass="";
+    pass=v1+v2+v3+v4;
+
+   return pass;
 }
 
 function getRandomLower() {
+   let l="";
+  let str="abcdefghijklmnopqrstuvwxyz";
+
+  for(let i=0;i<5;i++)
+  {
+    l+=str[Math.floor(Math.random()*26)]
+  }
+
+   return l;
 }
 
 function getRandomUpper() {
+    let u="";
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    for(let i=0;i<5;i++)
+    {
+      u+=str[Math.floor(Math.random()*26)]
+    }
+    return u;
 }
 
 function getRandomNumber() {
+    let n="";
+    let str="0123456789"
+
+    for(let i=0;i<5;i++)
+    {
+        n+=str[Math.floor(Math.random()*10)]
+    }
+    return n;
 }
 
 function getRandomSymbol() {
+    let s="";
+    let str="~@#$%^&*<>?,./';";
+
+    for(let i=0;i<5;i++)
+    {
+        s+=str[Math.floor(Math.random()*16)]
+    }
+    return s;
 }
