f03f6b30fb83342da1788d261741d727852e95c9
Shailesh <shaileshtripathi511@gmail.com>
2022-09-26 23:45:59 +0530

    Project-7 js added

---
+++ 07QRCodeGenerator - API/App.js
@@ -0,0 +1,6 @@
+document.getElementById("submit").addEventListener(
+    "click",()=>{
+        document.querySelector(".data").value="";
+        document.getElementById("img").style.display="block";
+    }
+)
